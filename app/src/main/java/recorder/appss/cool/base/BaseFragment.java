8e3139d10c5ac9784440d4ae26dcfe5cbac12c86
Yassin B <yassin123441@gmail.com>
2017-11-08 17:41:51 +0100

    Yassin Changes

--- app/src/main/java/recorder/appss/cool/base/BaseFragment.java
+++ app/src/main/java/recorder/appss/cool/base/BaseFragment.java
@@ -1,8 +1,46 @@
 package recorder.appss.cool.base;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by yassin baccour on 07/11/2017.
  */
 
-public class BaseFragment {
+public abstract class BaseFragment extends Fragment {
+
+    public abstract int getFragmentId();  //TODO Methode abstraite tu va la redefinir dans chaque fragment
+    protected View rootView;
+    protected Activity mActivity;
+    protected Context mContext;
+
+    @Override
+    public void onAttach(Context context) {
+        mActivity = (Activity) context;
+        mContext = context;
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(getFragmentId(), null);  //TODO maintenant un seul onCreateView et tu supprime oncreate view dans chaque frahment qui etend le base
+        return rootView;   //TODO tu redifini getFragmentId() dans chaque fragment qui donnera " return R.id.nom_du_view"
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+     }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
 }
