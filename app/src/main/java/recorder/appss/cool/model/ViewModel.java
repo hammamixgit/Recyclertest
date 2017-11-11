8713979cd10342455fdb8aaa89451fea28aef613
Yassin B <yassin123441@gmail.com>
2017-11-11 22:21:56 +0100

    Yassin Changes

--- app/src/main/java/recorder/appss/cool/model/ViewModel.java
+++ app/src/main/java/recorder/appss/cool/model/ViewModel.java
@@ -9,22 +9,24 @@ import recorder.appss.cool.utils.FileUtils;
  */
 
 public class ViewModel {
-    public static volatile ViewModel Current=null;  //TODO singleton (ViewModel Global)
+    public static volatile ViewModel Current = null;  //TODO singleton (ViewModel Global)
 
-    static  public FileUtils fileUtils;
-    static public DeviceUtils device;
-    static   public DataUtils dataUtils;
+    public FileUtils fileUtils; //TODO ne doivent pas etre static
+    public DeviceUtils device; //TODO ne doivent pas etre static
+    public DataUtils dataUtils; //TODO ne doivent pas etre static
 
     public ViewModel(DeviceUtils device, FileUtils fileUtils, DataUtils dataUtils) {
         this.device = device;
         this.fileUtils = fileUtils;
         this.dataUtils = dataUtils;
-        ViewModel.Current = getInstance() ;
+        ViewModel.Current = getInstance();  //TODO l'intance est crée dans la classe application
     }
-    public ViewModel(){
+
+    public ViewModel() {
         super();
     }
-    public final static ViewModel getInstance() {
+
+    public final static ViewModel getInstance() { //TODO a supprimer l'instance ce crée toujours dans ScoreApplication
         //Le "Double-Checked Singleton"/"Singleton doublement vérifié" permet
         //d'éviter un appel coûteux à synchronized,
         //une fois que l'instanciation est faite.
@@ -32,7 +34,7 @@ public class ViewModel {
             // Le mot-clé synchronized sur ce bloc empêche toute instanciation
             // multiple même par différents "threads".
             // Il est TRES important.
-            synchronized(ViewModel.class) {
+            synchronized (ViewModel.class) {  //TODO synchronise n'est pas trés utilise car y'a une seule instance a l'ouverture de l'application, si y'a beauoup d"instance dans des thread elle sera utile
                 if (ViewModel.Current == null) {
                     ViewModel.Current = new ViewModel();
                 }
